648271468fa577f3dcab6066a39efadbd6de2ba6
Julio Leon <juliopena143@icloud.com>
2023-01-24 14:46:52 -0700

    some more backend

--- Services/IngredientsService.cs
+++ Services/IngredientsService.cs
@@ -1,11 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Allspice.Services;
 
 public class IngredientsService
 {
-    
+    private readonly IngredientsRepository _repo;
+    private readonly RecipesService _recipesService;
+
+    public IngredientsService(IngredientsRepository repo, RecipesService recipesService)
+    {
+        _repo = repo;
+        _recipesService = recipesService;
+    }
+
+    internal Ingredient Create(Ingredient ingredientData)
+    {
+        Ingredient ingredient = _repo.Create(ingredientData);
+        return ingredient;
+    }
 }
